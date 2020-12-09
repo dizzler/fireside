c2295be742ad3fff4c2917b2eb2e60db95917d28
Nathan Randall <nathan.randall@fireeye.com>
2020-12-09 13:41:00 +0000

    Convert pipeline from simple to branching type
    Basic conversion of existing three-stage pipeline from a simple
    (but limited) linear format to a more complex (but more powerful)
    branching format.

--- pkg/pipeline/pipeline.go
+++ pkg/pipeline/pipeline.go
@@ -1,55 +1,71 @@
 package fireside
 
 import (
-	configure "fireside/pkg/configure"
-        envoy_logs "fireside/pkg/envoy/accesslog"
-	outproc "fireside/pkg/output_processors"
-	transform "fireside/pkg/transformers"
+    configure "fireside/pkg/configure"
+    envoy_logs "fireside/pkg/envoy/accesslog"
+    outproc "fireside/pkg/output_processors"
+    transform "fireside/pkg/transformers"
 
-        log "github.com/sirupsen/logrus"
+    log "github.com/sirupsen/logrus"
 
-        "github.com/dailyburn/ratchet"
+    "github.com/dailyburn/ratchet"
 )
 
 var (
-        awsCheckCerts   bool = false
-        awsOutputConfig *outproc.AwsOutputConfig
-        outputConfig    *outproc.OutputConfig
+    awsCheckCerts   bool = false
+    awsOutputConfig *outproc.AwsOutputConfig
+    outputConfig    *outproc.OutputConfig
 )
 
 func CreateEventPipeline(config *configure.Config) {
-        // Set the various *Config values used throughout the data processing pipeline
-        awsOutputConfig = &outproc.AwsOutputConfig{
-		Profile: config.Outputs.AWS.Profile,
-                Region: config.Outputs.AWS.Region,
-                AccessKeyID: config.Outputs.AWS.AccessKeyId,
-                SecretAccessKey: config.Outputs.AWS.SecretAccessKey,
-                S3BasePath: config.Outputs.AWS.S3.BasePath,
-		S3Bucket: config.Outputs.AWS.S3.Bucket}
-        outputConfig = &outproc.OutputConfig{
-                AWS: awsOutputConfig,
-                CheckCert: awsCheckCerts}
+    // Set the various *Config values used throughout the data processing pipeline
+    awsOutputConfig = &outproc.AwsOutputConfig{
+        Profile: config.Outputs.AWS.Profile,
+        Region: config.Outputs.AWS.Region,
+        AccessKeyID: config.Outputs.AWS.AccessKeyId,
+        SecretAccessKey: config.Outputs.AWS.SecretAccessKey,
+        S3BasePath: config.Outputs.AWS.S3.BasePath,
+        S3Bucket: config.Outputs.AWS.S3.Bucket}
+    outputConfig = &outproc.OutputConfig{
+        AWS: awsOutputConfig,
+        CheckCert: awsCheckCerts}
 
-	// Initialize the data extraction/input processors for pipeline
-        envoyAccesslogProc := envoy_logs.NewEnvoyAccesslogReader(config.Inputs.Envoy.Accesslog.Server.Port)
+    // Initialize the data extraction/input processors for pipeline
+    eventInEnvoy1 := envoy_logs.NewEnvoyAccesslogReader(config.Inputs.Envoy.Accesslog.Server.Port)
 
-        // Initialize the transformation/enrichment processors for the pipeline
-	var transformerSpec string = ""
-	transformerProc := transform.NewFormatter(transformerSpec)
+    // Initialize the transformation/enrichment processors for the pipeline
+    var transformerSpec string = ""
+    eventFmt1 := transform.NewFormatter(transformerSpec)
 
-	// Initialize the loading/exporting processors for the pipeline
-	// Send pipeline output to a directory on the local filesystem
-	cacheWriterProc := outproc.NewFsCacheWriter(
-		config.Outputs.Cache.Events.Directory,
-		config.Outputs.Cache.Events.Prefix,
-		outputConfig)
+    // Initialize the loading/exporting processors for the pipeline
+    // Send pipeline output to a directory on the local filesystem
+    eventOut1 := outproc.NewFsCacheWriter(
+        config.Outputs.Cache.Events.Directory,
+        config.Outputs.Cache.Events.Prefix,
+        outputConfig)
 
-        // Create a new pipeline using the initialized processors
-	pipeline := ratchet.NewPipeline(envoyAccesslogProc, transformerProc, cacheWriterProc)
+    // Create and validate the PipelineLayout for the complex, multi-phase "Events Pipeline"
+    layout, layerr := ratchet.NewPipelineLayout(
+        ratchet.NewPipelineStage(
+            ratchet.Do(eventInEnvoy1).Outputs(eventFmt1),
+        ),
+        ratchet.NewPipelineStage(
+            ratchet.Do(eventFmt1).Outputs(eventOut1),
+        ),
+        ratchet.NewPipelineStage(
+            ratchet.Do(eventOut1),
+        ),
+    )
+    if layerr != nil {
+        log.WithError(layerr).Fatal("failed to validate pipeline layout prior to creating data processing pipeline")
+    }
 
-	// Run the data processing pipeline and wait for either an error or nil to be returned
-	err := <-pipeline.Run()
-	if err != nil {
-                log.WithError(err).Fatal("error in data processing pipeline")
-	}
+    // Create a new pipeline using the initialized processors
+    pipeline := ratchet.NewBranchingPipeline(layout)
+
+    // Run the data processing pipeline and wait for either an error or nil to be returned
+    err := <-pipeline.Run()
+    if err != nil {
+        log.WithError(err).Fatal("error in data processing pipeline")
+    }
 }
--- pkg/transformers/formatter.go
+++ pkg/transformers/formatter.go
@@ -1,31 +1,31 @@
 package fireside
 
 import (
-        "github.com/dailyburn/ratchet/data"
-	kz "gopkg.in/qntfy/kazaam.v3"
-        log "github.com/sirupsen/logrus"
+    "github.com/dailyburn/ratchet/data"
+    kz "gopkg.in/qntfy/kazaam.v3"
+    log "github.com/sirupsen/logrus"
 )
 
 // Formatter is a struct that wraps the required inputs for a 'New' instance of kazaam
 // kazaam ia a library for arbitrary reformatting of JSON data
 //   https://github.com/qntfy/kazaam
 type Formatter struct {
-	Kz  *kz.Kazaam
+    Kz  *kz.Kazaam
 }
 
 // NewFormatter instantiates a new instance of Formatter
 func NewFormatter(spec string) *Formatter {
-	k, _ := kz.New(spec, kz.NewDefaultConfig())
-	return &Formatter{Kz: k}
+    k, _ := kz.New(spec, kz.NewDefaultConfig())
+    return &Formatter{Kz: k}
 }
 
 // ProcessData converts input JSON to a standard format / schema
 func (k *Formatter) ProcessData(d data.JSON, outputChan chan data.JSON, killChan chan error) {
-	dx, err := k.Kz.Transform(d)
-	if err != nil {
-                log.Error(err)
-	}
-	outputChan <- dx
+    dx, err := k.Kz.Transform(d)
+    if err != nil {
+        log.Error(err)
+    }
+    outputChan <- dx
 }
 
 // Finish - see interface for documentation.
@@ -33,5 +33,5 @@ func (k *Formatter) Finish(outputChan chan data.JSON, killChan chan error) {
 }
 
 func (k *Formatter) String() string {
-	return "Formatter"
+    return "Formatter"
 }
